7393263ea1e77b8978e523a58c03c277431299b4
Ian Rones <ronesp.85@gmail.com>
2019-11-21 08:32:31 -0800

    [add] superheroes.py file, included basic frameworkfor ability, armor, and hero class

---
+++ superheroes.py
@@ -0,0 +1,45 @@
+# superheroes.py
+import random
+
+# Ability Class
+
+class Ability:
+    def __init__(self,name,max_damage=0):
+        self.name = name
+        self.damage = max_damage
+    
+    def attack():
+        pass
+
+# Armor Class
+class Armor:
+    def __init__(self,name,max_block=0):
+        self.name = name
+        self.block = block
+
+    def block():
+        pass
+
+# Hero Class
+class Hero:
+    def __init__(self,name,starting_health=100):
+        self.name = name
+        self.health = starting_health
+
+    def add_ability(self,Ability):
+        pass
+
+    def attack(self):
+        pass
+
+    def defend(self,incoming_damage=0):
+        pass
+
+    def take_damage(self,damage):
+        pass
+
+    def is_alive(self):
+        pass
+
+    def fight(self,Hero):
+        pass
