18ec4bbea21b546b4d5d9d5496883bc7ac4b0d2d
Ian Rones <ronesp.85@gmail.com>
2019-11-21 08:17:27 -0800

    [add] my_dogs.py, including new instances of dogs and two new functions for Dog class

--- dog.py
+++ dog.py
@@ -1,9 +1,18 @@
 #dog.py
 
 class Dog:
-    def __init__(self,name):
-        self.dog_name = name
-        print('Dog Initiliazed!')
+    # Required properties that are defined in the __init__ constructor method
+    def __init__(self,dog_name,dog_breed):
+        self.name = dog_name
+        self.breed = dog_breed
+        # print('Dog Initiliazed!')
 
-#Instantiation call that creates a Dog object:
-Dog("Rexi")
+    def bark(self):
+        print(f'{self.name} says woooooof!')
+
+    def sit(self):
+        print(f'{self.name} sits.')
+
+    def roll(self):
+        print(f'{self.name} rolls over.')
+
---
+++ my_dogs.py
@@ -0,0 +1,13 @@
+# my_dogs.py
+import dog
+
+my_dog = dog.Dog('Rex','Kryptonian')
+my_other_dog = dog.Dog('Annie','Labrador')
+pitbull = dog.Dog('King','Pitbull')
+husky = dog.Dog('Thunder','Husky')
+shitzu = dog.Dog('Mochi','Shitzu')
+print(my_other_dog.name)
+my_dog.bark() # each dog will make the same sound because of the function bark()
+pitbull.bark()
+husky.sit()
+shitzu.roll()
