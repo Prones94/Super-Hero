5a124bb8277f157df713880f9b4e4ef47b20a03f
Ian Rones <ronesp.85@gmail.com>
2019-11-28 16:48:31 -0800

    Added animal.py file to learn about inheritance

---
+++ animal.py
@@ -0,0 +1,20 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self):
+        print(f'{self.name} is eating')
+
+    def drink(self):
+        print(f'{self.name} is drinking')
+
+class Frog(Animal):
+    def jump(self):
+        print(f'{self.name} is jumping!')
+
+if __name__ == "__main__":
+    tiger = Animal("Bob")
+    frog = Frog("Bill")
+    tiger.eat()
+    tiger.drink()
+    frog.jump()
